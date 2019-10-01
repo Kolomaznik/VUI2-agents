604606dd348440a0abb96b00bf04619f688d1320
Tibor Dudlák <tdudlak@redhat.com>
2019-10-01 15:10:34 +0200

    Add base for reactive agent

--- src/main/java/cz/mendelu/vui2/agents/Action.java
+++ src/main/java/cz/mendelu/vui2/agents/Action.java
@@ -2,5 +2,16 @@ package cz.mendelu.vui2.agents;
 
 public enum Action {
 
-    FORWARD, TURN_LEFT, TURN_RIGHT, CLEAN, TURN_OFF
+    FORWARD('F'),
+    TURN_LEFT('L'),
+    TURN_RIGHT('R'),
+    CLEAN('C'),
+    TURN_OFF('F');
+
+
+    public final char code;
+
+    Action(char code){
+        this.code = code;
+    }
 }
--- src/main/java/cz/mendelu/vui2/agents/ReactionAgent.java
+++ src/main/java/cz/mendelu/vui2/agents/ReactionAgent.java
@@ -1,11 +1,38 @@
 package cz.mendelu.vui2.agents;
 
 import cz.mendelu.vui2.agents.greenfoot.AbstractAgent;
+import java.util.List;
+import java.util.ArrayList;
 
 public class ReactionAgent extends AbstractAgent {
 
+    public interface Rule {
+        /**
+         *
+         */
+        Action match(boolean isWall, boolean dirty, boolean dock, String actions);
+    }
+
+    private List<Rule> rules = new ArrayList<>();
+    private String actions = "";
+
+    public ReactionAgent(){
+        rules.add((isWall, dirty, dock, actions)->(dock && (actions.length() > 250)) ? Action.TURN_OFF : null);
+        rules.add((isWall, dirty, dock, actions)->(!isWall) ? Action.FORWARD : null);
+        rules.add((isWall, dirty, dock, actions)->(dirty) ? Action.CLEAN : null);
+        rules.add((isWall, dirty, dock, actions)->(isWall && (actions.length() > 1)) ? Action.TURN_LEFT : null);
+    }
+
     @Override
-    public Action doAction(boolean canMove, boolean dirty, boolean dock) {
-        return null;
+    public Action doAction(boolean isWall, boolean dirty, boolean dock) {
+        for (Rule rule : rules) {
+            Action a = rule.match(isWall, dirty, dock, actions);
+            if (a != null) {
+                actions += a.code;
+                return a;
+            }
+        }
+        actions = "";
+        return Action.TURN_OFF;
     }
 }
